2a9555fd8fbaef4150f8349bb24c9554befe211d
amberm <nekosenpai@gmail.com>
2019-03-01 10:56:50 -0700

    well, i got everything done but the combo breaker...

--- apiwidget/server.js
+++ apiwidget/server.js
@@ -15,6 +15,52 @@ hardserver.get('/', (rec, rez) => {
     rez.send ({message: 'Well, the easy part is done, because you\'re seeing this'})
 })
 
+// get tests
+
+hardserver.get('/api/project', (req, res) => {
+    deebee('project')
+    .then( stuff => {
+      res.status(200).json(stuff)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+  });
+
+  
+hardserver.get('/api/action', (req, res) => {
+    deebee('action')
+    .then( stuff => {
+      res.status(200).json(stuff)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+  });
+
+// now for the 'fun' parts...
+
+hardserver.post('/api/project', (rec, rez) =>{
+    deebee('project')
+    .insert(rec.body)
+    .then( thing => {
+        rez.status(200).json(thing)
+    })
+    .catch(err =>{
+        rez.status(500).json(err)
+    })
+})
+
+hardserver.post('/api/action', (rec, rez) =>{
+    deebee('action')
+    .insert(rec.body)
+    .then( dothething =>{
+        rez.status(200).json(dothething)
+    })
+    .catch(err =>{
+        rez.status(500).json(err)
+    })
+})
 
 
 module.exports = hardserver;
